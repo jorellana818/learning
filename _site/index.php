28fcaff8420db6c931279115f770a917cd9b56ae
Jason Orellana <jorellana818@gmail.com>
2018-01-29 18:58:47 -0800

    Save my work

---
+++ _site/index.php
@@ -0,0 +1,139 @@
+ <!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<title></title>
+<!-- Add icon library -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link href="style.css" rel="stylesheet"/>
+</head>
+<body>
+<header>
+<nav class="navbar navbar-default navbar-fixed-top">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-navbar-collapse-1" aria-expanded="false">
+			<span class="sr-only">Toggle navigation</span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		  </button>
+		  <a class="navbar-brand" href="index.html"><img class="img-responsive" src="images/JasonOrellana_logo_red.png" alt="Jason Orellana name logo"/></a>
+		</div>
+		<div class="collapse navbar-collapse flex-nav" id="bs-navbar-collapse-1">
+			<ul class="nav navbar-nav navbar-right">
+			  <li><a href="#about">About</a></li>
+			  <li><a href="#portfolio">Portfolio</a></li>
+			  <li><a href="#contact">Contact</a></li>
+			</ul>
+		</div>
+	</div>	
+</nav>
+</header>
+<section id="about" class="flex flex-center">
+	<div class="col-8 about-p">
+		<h2>Web Developer</h2>
+		<div id="wave_container">
+			<canvas id="waves"></canvas>
+		</div>
+		<h3>Hello! I am glad you have found me!</h3>
+		<p>I am an up-and-coming Web Developer who is obsessed with learning code and creating things. I worked in the import business for nine and a half years but I always had a desire to challenge myself more intellectually and creatively. In 2014 I began studying software development and was instantly hooked. I graduated as Valedictorian of the Software Developement class from ITT Technical Institute in 2016. I am currently working on a handful of freelance projects while continuing to improve skills with a lot of online learning. </p>
+	</div>
+	<div class="col-4 flex flex-center flex-align-center">
+		<img id="about_pic" class="images" src="images/Jason_Orellana_1.png" alt="Jason Orellana picture 1"/>
+	</div>
+	
+</section>
+<section class="skill">
+	<h2>Skills At A Glance:</h2>
+	<div class="skills flex flex-col-center flex-wrap">
+		<div>HTML</div>
+		<div>CSS</div>
+		<div>JavaScript</div>
+		<div>jQuery</div>
+		<div>Bootstrap</div>
+		<div>WordPress</div>
+		<div>Photoshop</div>
+		<div>PHP</div>
+		<div>MySql</div>
+		<div>Java</div>
+		<div>Android</div>
+	</div>
+</section>
+
+<section id="portfolio" >
+	<div class="flex flex-col-center">
+		<h2>Portfolio</h2>
+		<div class="just_a_line"></div>
+		<div class="flex flex-center items_div" >
+			<a href="https://oakfencecongregate.org/" class="portfolio_link flex flex-col-center" target="_blank">
+				<p>oakfencecongregate.org</p>
+				<img class="images" src="images/oak_fence_congregate.jpg" alt="screenshot Oak Fence Congregate Website"/>
+			</a>
+			<a href="http://www.emphasis.la" class="portfolio_link flex flex-col-center" target="_blank">
+				<p>emphasis.la</p>
+				<img class="images" src="images/emphasis_la.jpg" alt="screenshot Emphasis LA website"/>
+			</a>
+			<a href="http://bandofbrothersandsisters.net/" class="portfolio_link flex flex-col-center" target="_blank">
+				<p>bandofbrothersandsisters.net</p>
+				<img class="images" src="images/band_of_brothers_and_sisters.jpg" alt="screenshot Band of Brothers and Sisters Website"/>
+			</a>
+			<a href="http://highlandparkfullgospelassembly.com/" class="portfolio_link flex flex-col-center" target="_blank">
+				<p>highlandparkfullgospelassembly.com</p>
+				<img class="images" src="images/Highland_Park_full_gospel.jpg" alt="screenshot HP Full Gospel Assembly Website"/>
+			</a>
+			<a href="http://bitjourni.net/" class="portfolio_link flex flex-col-center" target="_blank">
+				<p>bitjourni.net</p>
+				<img class="images" src="images/bitjourni.jpg" alt="screenshot Bitjourni website"/>
+			</a>
+		</div>
+		<a style="background-color:transparent;color:white;text-decoration:none;padding:4px 6px;font-family:-apple-system, BlinkMacSystemFont, &quot;San Francisco&quot;, &quot;Helvetica Neue&quot;, Helvetica, Ubuntu, Roboto, Noto, &quot;Segoe UI&quot;, Arial, sans-serif;font-size:12px;font-weight:bold;line-height:1.2;display:inline-block;border:3px solid #fff;border-radius:7px;" href="https://unsplash.com/@cam_bam?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge" target="_blank" rel="noopener noreferrer" title="Download free do whatever you want high-resolution photos from Camille Kimberly"><span style="display:inline-block;padding:2px 3px;"><svg xmlns="http://www.w3.org/2000/svg" style="height:16px;width:auto;position:relative;vertical-align:middle;top:-1px;fill:white;" viewBox="0 0 32 32"><title></title><path d="M20.8 18.1c0 2.7-2.2 4.8-4.8 4.8s-4.8-2.1-4.8-4.8c0-2.7 2.2-4.8 4.8-4.8 2.7.1 4.8 2.2 4.8 4.8zm11.2-7.4v14.9c0 2.3-1.9 4.3-4.3 4.3h-23.4c-2.4 0-4.3-1.9-4.3-4.3v-15c0-2.3 1.9-4.3 4.3-4.3h3.7l.8-2.3c.4-1.1 1.7-2 2.9-2h8.6c1.2 0 2.5.9 2.9 2l.8 2.4h3.7c2.4 0 4.3 1.9 4.3 4.3zm-8.6 7.5c0-4.1-3.3-7.5-7.5-7.5-4.1 0-7.5 3.4-7.5 7.5s3.3 7.5 7.5 7.5c4.2-.1 7.5-3.4 7.5-7.5z"></path></svg></span><span style="display:inline-block;padding:2px 3px;">Camille Kimberly</span></a>
+	</div>
+</section>
+<section id="contact" class="flex flex-col-center">
+	<h2>Contact</h2>
+	<div class="just_a_line"></div>
+	<div class="flex flex-center items_div" >
+		<div class="col-5">
+			<form id="contact_form" class="flex flex-col-left flex-wrap" action="form_processing.php" method="post">
+				<input name="fname" type="text" placeholder="  First Name  " tabindex="1" required/>
+				<input name="lname" type="text" placeholder="  Last Name  " tabindex="1" required/>
+				<input name="email" type="email" placeholder="  Email  " tabindex="1" required/>
+				<input name="phone" type="tel" placeholder="  Phone Number  " tabindex="1" />
+				<textarea name="message" type="text" placeholder="  Type your message here  " tabindex="1" required></textarea>
+				<button name="submit" type="submit" id="contact-submit">  Send  </button>
+			</form>
+			
+		</div>
+		<div class="col-5">
+			<p><strong>Phone: </strong>(818) 200-5491</p>
+			<p><strong>Email: </strong>info.jasonorellana@gmail.com</p>
+			<p><strong>Website: </strong><a href="http://www.jasonorellana.net/" target="_blank">jasonorellana.net</a></p>
+			<div class="social_media">
+				<a href="https://www.facebook.com/jorellana818" class="fa fa-facebook fa-fw fa-2x" target="_blank"></a>
+				<a href="https://twitter.com/jorellana818" class="fa fa-twitter fa-fw fa-2x" target="_blank"></a>
+				<a href="https://www.linkedin.com/in/jorellana818/" class="fa fa-linkedin fa-fw fa-2x" target="_blank"></a>
+				<a href="https://plus.google.com/u/0/107384512428292454582" class="fa fa-google-plus fa-fw fa-2x" target="_blank"></a>
+				<a href="https://www.freecodecamp.org/jorellana818" class="fa fa-free-code-camp fa-fw fa-2x" target="_blank"></a>
+				<a href="https://www.instagram.com/jorellana818/" class="fa fa-instagram fa-fw fa-2x" target="_blank"></a>
+			</div>
+		</div>
+	</div>
+</section>
+
+<footer class="flex flex-center flex-align-center">
+	<p>&copy; Jason Orellana 2017</p>
+</footer>
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<!-- <script src="https://isuttell.github.io/sine-waves/javascripts/sine-waves.min.js"></script> -->
+<script src="portfolio.js"></script>
+</body>
+</html>
